7e6c536656edf6b85dc6ce7ec8e9c0dd6a63a188
John W. Fail <john@pixelache.ac>
2016-10-22 15:37:32 +0300

    aa custom

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module Copacetic
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
    # actionmailer
   # Disable delivery errors, bad email addresses will be ignored
   end
