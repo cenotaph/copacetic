65e28c443b80cb061aa88a72ad8d57f87d428352
John W. Fail <john@pixelache.ac>
2016-10-30 23:01:29 +0200

    oh yeah

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -19,14 +19,14 @@ class SearchesController < ApplicationController
     @items = ThinkingSphinx.search ThinkingSphinx::Query.escape(params[:query]), :classes => [Comic, Book, Dvd, Cd], field_weights: {
         title: 10,
         description: 5 }
-    creators_and_directors = ThinkingSphinx.search ThinkingSphinx::Query.escape(params[:query]), :classes => [Director, Creator],  field_weights: {
-        lastname: 10,
-        firstname: 8, description: 5 }
-    unless creators_and_directors.empty?
-      other_items = creators_and_directors.map(&:items).uniq
-    end
+    # creators_and_directors = ThinkingSphinx.search ThinkingSphinx::Query.escape(params[:query]), :classes => [Director, Creator],  field_weights: {
+    #     lastname: 10,
+    #     firstname: 8, description: 5 }
+    # unless creators_and_directors.empty?
+    #   other_items = creators_and_directors.map(&:items).uniq
+    # end
 
-    @items = Kaminari.paginate_array([@items, other_items].flatten.compact.uniq).page(params[:page]).per(32)
+    @items = Kaminari.paginate_array(@items).page(params[:page]).per(32)
     if !request.xhr?
       render 'shared/search_results'
     end
